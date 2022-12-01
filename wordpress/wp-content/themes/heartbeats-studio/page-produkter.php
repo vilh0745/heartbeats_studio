32a8a09ece0202b707db363f6dbf4937e301d304
Vilhelm Saxild <vilh0745@stud.kea.dk>
2022-12-01 14:48:22 +0100

    filtrering

--- wordpress/wp-content/themes/heartbeats-studio/page-produkter.php
+++ wordpress/wp-content/themes/heartbeats-studio/page-produkter.php
@@ -11,18 +11,40 @@
  get_header();
  ?>
 
+ <style>
+    #main {
+        display: grid;
+        grid-template-columns: 10vw 80vw;
+    }
+
+    #case_oversigt {
+        grid-column: 2;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        gap: 20px;
+        width: 90vw;
+        max-width: 1580px;
+        margin: 0 auto;
+      }
+
+      img {
+        width: 100%;
+        height: auto;
+        display: block;
+      }
+ </style>
+
 
 <section id="primary" class="content-area">
     <main id="main" class="site-main">
 
-    <h1 id="overskrift">Cases</h1>
-    <section id="case_oversigt"></section>
+<section id="case_oversigt"></section>
 </main>
 <template>
     <article>
         <img src="" alt="">
-        <h2></h2>
-        <h3></h3>
+        <h5></h5>
+        <h6></h6>
     </article>
 </template>
 
@@ -46,15 +68,20 @@
 
     function visProdukter() {
         console.log(produkter);
+        liste.innerHTML = "";
         produkter.forEach(produkt => {
+            if (filterProdukt == "alle" || produkt.categories.includes(parseInt(filterProdukt))) {
             const klon = skabelon.cloneNode(true).content;
             klon.querySelector("img").src = produkt.billede.guid;
-            klon.querySelector("h2").innerHTML = produkt.titel.rendered;
-            klon.querySelector("h3").innerHTML = produkt.produkttype.rendered;
-
+            klon.querySelector("h5").innerHTML = produkt.titel;
+            klon.querySelector("h6").innerHTML = produkt.produkttype;
+            klon.querySelector("article").addEventListener("click", () => {
+                location.href = produkt.link;
+            })
             liste.appendChild(klon);
-        })
-    }
+        }
+    })
+}
 </script>
 
 <?php
---
+++ wordpress/wp-content/themes/heartbeats-studio/single-produkt.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<section id="primary" class="content-area">
+	<main id="main" class="site-main">
+
+	<button class="luk">Tilbage</button>
+	<section class="indhold">
+		<article class="enkeltProdukt">
+			<img id="pic" src="" alt="">
+			<p class="text"></p>
+		</article>
+	</section>
+
+</main>
+
+<script>
+	let produkt;
+	document.addEventListener("DOMContentLoaded", getJson);
+
+	async function getJson() {
+		console.log("id er", <?php echo get_the_ID() ?>);
+
+		let jsonData = await fetch(`http://vilhelmsaxild.com/kea/heartbeats_studio/wordpress/wp-json/wp/v2/produkt/<?php echo get_the_ID() ?>`);
+		produkt = await jsonData.json();
+		visProdukt();
+	}
+
+	function visProdukt() {
+		document.querySelector("h2").innerHTML = produkt.titel;
+		document.querySelector(".text").innerHTML = produkt.produkttype;
+		document.querySelector("#pic").src = produkt.billede.guid;
+	}
+
+			document.querySelector(".luk").addEventListener("click", () => {
+				history.back();
+			});
+</script>
+
+</section>
+
+<?php
+get_footer();
