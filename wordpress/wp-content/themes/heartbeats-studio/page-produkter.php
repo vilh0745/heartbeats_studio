5897b412e32e9de727540d666029b403b5f44138
Vilhelm Saxild <vilh0745@stud.kea.dk>
2022-12-01 09:45:11 +0100

    nyt tema

--- wordpress/wp-content/themes/heartbeats-studio/page-produkter.php
+++ wordpress/wp-content/themes/heartbeats-studio/page-produkter.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
 get_header();
 ?>
